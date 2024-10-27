a4d7dfe6c78129ad50e8e088fce6f713a3e73fcc
abdulaziz <aboudecharkawi@gmail.com>
2024-10-27 14:42:26 +0200

    logout

--- Middlewares/authentication.js
+++ Middlewares/authentication.js
@@ -82,7 +82,7 @@ export const authenticateUser = (req, res, next) => {
 
   export const logOut = (req, res) => {
     return res
-      .clearCookie("token", {sameSite:true, secure:true})
+      .clearCookie("token", {sameSite:none, secure:true})
       .status(200)
       .json({ message: "Successfully Logged Out!" });
   };
